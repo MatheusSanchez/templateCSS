06ad629b8cc1903e23be2516a3d44aaf75fdc0f6
matheussanchez <matheus.sanchez@ifood.com.br>
2021-05-11 15:12:13 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from './styles/global'
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
